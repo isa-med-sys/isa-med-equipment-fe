4bc26bd8da770c7abefa57f9933cb89d3a22e29b
nina <ninabu16@gmail.com>
2023-11-17 03:03:41 +0100

    feat: add model interfaces for registered user

--- med-equipment/src/app/layout/navbar/navbar.component.ts
+++ med-equipment/src/app/layout/navbar/navbar.component.ts
@@ -5,7 +5,7 @@ import { User } from 'src/app/authentication/model/user.model';
 @Component({
   selector: 'xp-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
   
