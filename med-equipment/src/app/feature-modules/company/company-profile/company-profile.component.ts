0589e69d72f42fbc553e1a380ab639dea8129bcc
BrziGolub <petarkovacevic58@gmail.com>
2023-11-19 18:35:09 +0100

    fix: equipment in company profile

--- med-equipment/src/app/feature-modules/company/company-profile/company-profile.component.ts
+++ med-equipment/src/app/feature-modules/company/company-profile/company-profile.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Company } from "../../../shared/model/company";
 import { CompanyService } from "../company.service";
 import { ActivatedRoute } from "@angular/router";
+import { Equipment } from "../../../shared/model/equipment";
 
 @Component({
   selector: 'app-company-profile',
@@ -12,6 +13,7 @@ export class CompanyProfileComponent {
 
   companyId!: number;
   company!: Company;
+  equipment: Equipment[] = [];
 
   constructor(private route: ActivatedRoute, private companyService: CompanyService) {
   }
@@ -24,6 +26,15 @@ export class CompanyProfileComponent {
     this.companyService.getCompanyById(this.companyId).subscribe({
       next: (company) => {
         this.company = company;
+
+        //if(this.userRole) {
+          this.companyService.getEquipment(this.companyId).subscribe({
+            next: (result) => {
+              this.equipment = result;
+              console.log(this.equipment);
+            }
+          });
+        //}
       },
       error: (err) => {
         console.error('Error fetching company:', err);
--- med-equipment/src/app/feature-modules/company/company.service.ts
+++ med-equipment/src/app/feature-modules/company/company.service.ts
@@ -4,6 +4,7 @@ import { Observable } from "rxjs";
 import { Company } from "../../shared/model/company";
 import { environment } from "../../../env/environment";
 import { PagedResults } from 'src/app/shared/model/paged-results.model';
+import { Equipment } from "../../shared/model/equipment";
 
 @Injectable({
   providedIn: 'root'
@@ -16,20 +17,24 @@ export class CompanyService {
     const params = this.buildParams(name, city, rating, page, size);
     return this.http.get<PagedResults<Company>>(environment.apiHost + 'companies', { params });
   }
-  
-  getCompanyById(id: number):Observable<Company> {
+
+  getCompanyById(id: number): Observable<Company> {
     return this.http.get<Company>(environment.apiHost + `companies/${id}`);
+  }
+
+  getEquipment(id: number): Observable<Equipment[]> {
+    return this.http.get<Equipment[]>(environment.apiHost + `companies/all-equipment/${id}`);
   }
 
   private buildParams(name: string, city: string, rating: number, page: number, size: number): HttpParams {
     let params = new HttpParams()
       .set('page', page.toString())
       .set('size', size.toString());
-  
+
     if (name) params = params.set('name', name);
     if (city) params = params.set('city', city);
     if (rating) params = params.set('rating', rating.toString());
-  
+
     return params;
   }
 }
