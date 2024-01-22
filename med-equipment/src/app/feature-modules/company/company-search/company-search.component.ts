60c305a06c267a786c8f69a8a89d01b6b477c10f
nina <ninabu16@gmail.com>
2024-01-23 00:32:17 +0100

    feat: search companies

--- med-equipment/src/app/feature-modules/company/company-search/company-search.component.ts
+++ med-equipment/src/app/feature-modules/company/company-search/company-search.component.ts
@@ -1,10 +1,11 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Company } from 'src/app/shared/model/company';
 import { CompanyService } from '../company.service';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSort, SortDirection } from '@angular/material/sort';
 
 @Component({
   selector: 'app-company-search',
@@ -24,10 +25,18 @@ export class CompanySearchComponent implements AfterViewInit {
   name: string = '';
   city: string = '';
   rating: number = 0;
+  sortField: string = 'name';
+  sortDirection: string = 'asc'; 
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private companyService: CompanyService, private router: Router, private formBuilder: FormBuilder) {
+  constructor(
+    private companyService: CompanyService,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private cdr: ChangeDetectorRef
+  ) {
     this.dataSource = new MatTableDataSource<Company>();
     this.searchForm = this.formBuilder.group({
       name: [''],
@@ -37,11 +46,30 @@ export class CompanySearchComponent implements AfterViewInit {
   }
 
   ngAfterViewInit(): void {
+    this.sort.direction = this.sortDirection as SortDirection;
+    this.sort.active = this.sortField;
+
+    this.sort.sortChange.subscribe(() => {
+      this.sortField = this.sort.active;
+      this.sortDirection = this.sort.direction as string;
+      this.loadCompanies();
+    });
+
+    this.cdr.detectChanges();
     this.loadCompanies();
   }
 
+
   loadCompanies(): void {
-    this.companyService.getCompanies(this.name, this.city, this.rating, this.page, this.size).subscribe(result => {
+    this.companyService.getCompanies(
+      this.name,
+      this.city,
+      this.rating,
+      this.page,
+      this.size,
+      this.sortField,
+      this.sortDirection
+    ).subscribe(result => {
       this.dataSource = new MatTableDataSource<Company>();
       this.dataSource.data = result.content;
       this.totalCompanies = result.totalElements;
